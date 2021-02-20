4b89f393ea0ae635bb1574273c5e5cf50edbbc39
Dudo360 <dudoguy360@gmail.com>
2021-02-20 02:20:38 -0500

    Event Folder.
    Folder for events. Again Hardly any coding, just the file.

---
+++ toontown/events/ResistenceTasks.py
@@ -0,0 +1,31 @@
+#Sellbots
+
+Hollywood = 'mh'
+Mingler = 'm'
+TwoFace = 'tf'
+
+#Cashbots
+Barron = 'rb'
+Shark = 'ls'
+Bags = 'mb'
+
+#Lawbots
+Wig = 'bw'
+Eagle = 'le'
+Doctor = 'sd'
+
+#Bossbots
+BigCheese = 'tbc'
+Raider = 'cr'
+Hunter = 'hh'
+
+#Levels
+
+Tier1Level = 8
+Tier2Level = 9
+Tier3Level = 10
+Tier4Level = 11
+Tier5Level = 12
+Tier5ExtendedLevel = 13
+Tier6Level = 14
+
